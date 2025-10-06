6d310286d7fccf514bfeb80a2fbb25aa84733017
Križan Šabić <krizan.sabic@techresources.hr>
2025-10-07 00:12:59 +0200

    Wallet balance check migration added

---
+++ BettingPlatform.Infrastructure/Persistence/Migrations/20251006221229_WalletBalanceCheck.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BettingPlatform.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class WalletBalanceCheck : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PayoutAmount",
+                table: "Tickets",
+                type: "decimal(14,2)",
+                precision: 14,
+                scale: 2,
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "FeePercent",
+                table: "Tickets",
+                type: "decimal(5,4)",
+                precision: 5,
+                scale: 4,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Wallet_Balance_NonNegative",
+                table: "Wallets",
+                sql: "[Balance] >= 0");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Wallet_Balance_NonNegative",
+                table: "Wallets");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PayoutAmount",
+                table: "Tickets",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(14,2)",
+                oldPrecision: 14,
+                oldScale: 2,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "FeePercent",
+                table: "Tickets",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(5,4)",
+                oldPrecision: 5,
+                oldScale: 4);
+        }
+    }
+}
